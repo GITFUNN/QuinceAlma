814b81080c5e06731f81ff69a55b3bed1e000b5f
GITFUNN <isamuga2003@gmail.com>
2024-04-12 11:42:22 -0300

    fixed background

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -202,7 +202,7 @@ const MainPage = () => {
       )}
       <div
         id="section0"
-        className="min-h-screen flex justify-center items-center bg-backAlma bg-fixed bg-contain bg-center bg-no-repeat"
+        className="min-h-screen flex justify-center items-center bg-backAlma bg-fixed bg-contain"
       >
         <div className="flex flex-col justify-center items-center ">
           <h1 className="text-8xl text-white sm:text-9xl select-none font-bodiee">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,30 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-      display : ['Madimi One', 'sans-serif'],
-      body:['Dancing Script','cursive'],
-      bodie:['Open Sans', 'sans-serif'],
-     bodiee:['Monsieur La Doulaise', 'cursive'],
-
+        display: ["Madimi One", "sans-serif"],
+        body: ["Dancing Script", "cursive"],
+        bodie: ["Open Sans", "sans-serif"],
+        bodiee: ["Monsieur La Doulaise", "cursive"],
       },
-      backgroundImage:{
-        'backAlma': 'url("/static/fotoAlma1.jpg")',
-        'photo1': 'url("/static/fotoAlma3.jpg")',
-        'photo2':'url("/static/fotoAlma4.jpg")',
-        'photo3': 'url("/static/fotoAlma5.jpg")',
-        'photo4': 'url("/static/fotoAlma6.jpg")',
-        'photo5': 'url("/static/fotoAlma8.jpg")',
-        'photo6': 'url("/static/fotoAlma9.jpg")',
-      }
-
+      backgroundImage: {
+        backAlma: 'url("/static/fotoAlma1.jpg")',
+        photo1: 'url("/static/fotoAlma3.jpg")',
+        photo2: 'url("/static/fotoAlma4.jpg")',
+        photo3: 'url("/static/fotoAlma5.jpg")',
+        photo4: 'url("/static/fotoAlma6.jpg")',
+        photo5: 'url("/static/fotoAlma8.jpg")',
+        photo6: 'url("/static/fotoAlma9.jpg")',
+      },
     },
   },
   plugins: [],
-}
-
+};
